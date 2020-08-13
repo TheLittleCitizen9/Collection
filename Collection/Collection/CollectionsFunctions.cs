dd2daf4d2430f32ffc3448cd63dec177e12c4b39
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-13 12:05:15 +0300

    Finished ListOperations function

---
+++ Collection/Collection/CollectionsFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collection
+{
+    public class CollectionsFunctions
+    {
+        public List<string> ListOperations(List<string> stringsList)
+        {
+            int middleIndex;
+            if(stringsList.Count % 2!= 0)
+            {
+                middleIndex = stringsList.Count / 2;
+                stringsList.RemoveAt(middleIndex);
+            }
+            if(stringsList.Contains("mamas"))
+            {
+                stringsList.Add("6");
+            }
+
+            if(stringsList.Contains("aram"))
+            {
+                int index = stringsList.IndexOf("aram");
+                if(index % 2 != 0)
+                {
+                    stringsList.Reverse();
+                }
+            }
+            List<string> str = new List<string>{ "2", "3", "4" };
+            var countOfDiffernetValues = stringsList.GroupBy(str => str).Count();
+            if(countOfDiffernetValues == 3)
+            {
+                stringsList.InsertRange(2, str);
+            }
+            return stringsList;
+            
+        }
+    }
+}
---
+++ Collection/Collection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CollectionsFunctions collectionsFunctions = new CollectionsFunctions();
+            List<string> lst = new List<string>{ "mamas", "6", "lior", "mamas", "aram"};
+            var newLst = collectionsFunctions.ListOperations(lst);
+            foreach (var item in newLst)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
